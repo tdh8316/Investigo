f87eed7663e33ddbefc0841ee8a62212cad3b9d6
탁동혁 <tdh8316@naver.com>
2019-01-01 22:24:06 +0900

    happy new year 😙

--- web/sniffing.go
+++ web/sniffing.go
@@ -1,36 +1,36 @@
 package web
 
 import (
-	"os"
-	"fmt"
-	"strings"
-	"io/ioutil"
-	"encoding/json"
+    "os"
+    "fmt"
+    "strings"
+    "io/ioutil"
+    "encoding/json"
 )
 
 var webURL map[string]interface{}
 
 func init() {
-	dataFile, err := os.Open("data.json")
-	if err != nil {
-		panic(err)
-	}
-	defer dataFile.Close()
+    dataFile, err := os.Open("data.json")
+    if err != nil {
+        panic(err)
+    }
+    defer dataFile.Close()
 
-	byteValue, _ := ioutil.ReadAll(dataFile)
-	json.Unmarshal([]byte(byteValue), &webURL)
+    byteValue, _ := ioutil.ReadAll(dataFile)
+    json.Unmarshal([]byte(byteValue), &webURL)
 }
 
 
 // Sniffer search username across social media
 func Sniffer(username string) {
-	for site := range webURL {
-		url := strings.Replace(webURL[site].(string), "?", username, 1)
-		
-		if IsUserExist(url, username, site) {
-			fmt.Printf("[+] %s: %s\n", site, url)
-		} else {
-			fmt.Printf("[-] %s: Not found\n", site)
-		}
-	}
+    for site := range webURL {
+        url := strings.Replace(webURL[site].(string), "?", username, 1)
+        
+        if IsUserExist(url, username, site) {
+            fmt.Printf("[+] %s: %s\n", site, url)
+        } else {
+            fmt.Printf("[-] %s: Not found\n", site)
+        }
+    }
 }
