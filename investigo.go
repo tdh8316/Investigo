b365fb73d8815284fcc7501513694ac6c9ac8982
탁동혁 <tdh8316@naver.com>
2019-08-11 16:26:39 +0900

    Update header `User-Agent`

--- investigo.go
+++ investigo.go
@@ -17,7 +17,7 @@ import (
 
 const (
 	dataFileName  string = "data.json"
-	userAgent     string = "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/75.0.3770.100 Safari/537.36"
+	userAgent     string = "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/76.0.3809.100 Safari/537.36"
 	maxGoroutines int    = 64
 )
 
