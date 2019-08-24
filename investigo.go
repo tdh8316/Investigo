521071e78c906bdf5eabda971c07d6d6a8abf7e8
lucmski <47478603+lucmski@users.noreply.github.com>
2019-08-24 04:15:15 +0200

    Merge pull request #6 from https://github.com/lucmski/Investigo/tree/dev

--- investigo.go
+++ investigo.go
@@ -6,13 +6,13 @@ import (
 	"io/ioutil"
 	"log"
 	"net/http"
+	"net/url"
 	"os"
 	"strings"
 	"sync"
-)
 
-import (
 	color "github.com/fatih/color"
+	"golang.org/x/net/proxy"
 )
 
 const (
@@ -21,13 +21,94 @@ const (
 	maxGoroutines int    = 64
 )
 
+// Initialize sites not included in Sherlock
+func initializeExtraSiteData() {
+	siteData["Pornhub"] = SiteData{
+		ErrorType: "status_code",
+		URLMain:   "https://www.pornhub.com/",
+		URL:       "https://www.pornhub.com/users/{}",
+	}
+	siteData["NAVER"] = SiteData{
+		ErrorType: "status_code",
+		URLMain:   "https://www.naver.com/",
+		URL:       "https://blog.naver.com/{}",
+	}
+	siteData["xvideos"] = SiteData{
+		ErrorType: "status_code",
+		URLMain:   "https://xvideos.com/",
+		URL:       "https://xvideos.com/profiles/{}",
+	}
+}
+
+func main() {
+	fmt.Println(`Investigo - Investigate User Across Social Networks.`)
+
+	args := os.Args[1:]
+	var argIndex int
+
+	options.noColor, argIndex = HasElement(args, "--no-color")
+	if options.noColor {
+		logger = log.New(os.Stdout, "", 0)
+		args = append(args[:argIndex], args[argIndex+1:]...)
+	}
+
+	options.withTor, argIndex = HasElement(args, "-t", "--tor")
+	if options.withTor {
+		args = append(args[:argIndex], args[argIndex+1:]...)
+	}
+
+	options.verbose, argIndex = HasElement(args, "-v", "--verbose")
+	if options.verbose {
+		args = append(args[:argIndex], args[argIndex+1:]...)
+	}
+
+	options.checkForUpdate, argIndex = HasElement(args, "--update")
+	if options.checkForUpdate {
+		args = append(args[:argIndex], args[argIndex+1:]...)
+	}
+
+	// Loads site data from sherlock database and assign to a variable.
+	initializeSiteData(options.checkForUpdate)
+
+	if help, _ := HasElement(args, "-h", "--help"); help || len(args) < 1 {
+		os.Exit(0)
+	}
+
+	// Loads extra site data
+	initializeExtraSiteData()
+
+	for _, username := range args {
+		if options.noColor {
+			fmt.Printf("Investigating %s on:\n", username)
+		} else {
+			fmt.Fprintf(color.Output, "Investigating %s on:\n", color.HiGreenString(username))
+		}
+		waitGroup.Add(len(siteData))
+		for site := range siteData {
+			guard <- 1
+			go func(site string) {
+				defer waitGroup.Done()
+				res := Investigo(username, site, siteData[site])
+				WriteResult(res)
+				<-guard
+			}(site)
+		}
+		waitGroup.Wait()
+	}
+	return
+}
+
 // Result of Investigo function
 type Result struct {
-	exist  bool
-	site   string
-	link   string
-	err    bool
-	errMsg string
+	Usernane string
+	Exist    bool
+	Proxied  bool
+	Site     string
+	URL      string
+	URLProbe string
+	Link     string
+	Err      bool
+	ErrMsg   string
 }
 
 var (
@@ -38,6 +119,9 @@ var (
 	options   struct {
 		noColor         bool
 		updateBeforeRun bool
+		withTor         bool
+		withAdmin       bool
+		withExport      bool
 		verbose         bool
 		checkForUpdate  bool
 	}
@@ -86,7 +170,7 @@ func initializeSiteData(forceUpdate bool) {
 			jsonFile.Close()
 		}
 
-		r, err := Request("https://github.com/sherlock-project/sherlock/blob/master/data.json")
+		r, err := Request("https://raw.githubusercontent.com/sherlock-project/sherlock/master/data.json")
 		if err != nil || r.StatusCode != 200 {
 			if options.noColor {
 				fmt.Printf(" [%s]\n", ("Failed"))
@@ -129,83 +213,39 @@ func initializeSiteData(forceUpdate bool) {
 	return
 }
 
-// Initialize sites not included in Sherlock
-func initializeExtraSiteData() {
-	siteData["Pornhub"] = SiteData{
-		ErrorType: "status_code",
-		URLMain: "https://www.pornhub.com/",
-		URL: "https://www.pornhub.com/users/{}",
-	}
-	siteData["NAVER"] = SiteData{
-		ErrorType: "status_code",
-		URLMain: "https://www.naver.com/",
-		URL: "https://blog.naver.com/{}",
-	}
-}
-
-func main() {
-	fmt.Println(`Investigo - Investigate User Across Social Networks.`)
-
-	args := os.Args[1:]
-	var argIndex int
-
-	options.noColor, argIndex = HasElement(args, "--no-color")
-	if options.noColor {
-		logger = log.New(os.Stdout, "", 0)
-		args = append(args[:argIndex], args[argIndex+1:]...)
-	}
-
-	options.verbose, argIndex = HasElement(args, "-v", "--verbose")
-	if options.verbose {
-		args = append(args[:argIndex], args[argIndex+1:]...)
-	}
-
-	options.checkForUpdate, argIndex = HasElement(args, "--update")
-	if options.checkForUpdate {
-		args = append(args[:argIndex], args[argIndex+1:]...)
-	}
-
-	// Loads site data from sherlock database and assign to a variable.
-	initializeSiteData(options.checkForUpdate)
-
-	if help, _ := HasElement(args, "-h", "--help"); help || len(args) < 1 {
-		os.Exit(0)
-	}
-
-	// Loads extra site data
-	initializeExtraSiteData()
-
-	for _, username := range args {
-		if options.noColor {
-			fmt.Printf("Investigating %s on:\n", username)
-		} else {
-			fmt.Fprintf(color.Output, "Investigating %s on:\n", color.HiGreenString(username))
-		}
-		waitGroup.Add(len(siteData))
-		for site := range siteData {
-			guard <- 1
-			go func(site string) {
-				defer waitGroup.Done()
-				WriteResult(
-					Investigo(username, site, siteData[site]),
-				)
-				<-guard
-			}(site)
-		}
-		waitGroup.Wait()
-	}
-
-	return
-}
+// Specify Tor proxy ip and port
+// var torProxy string = "socks5://127.0.0.1:9050" // 9150 w/ Tor Browser
+// var UseTor bool = true
 
 // Request makes HTTP request
-func Request(url string) (*http.Response, RequestError) {
-	request, err := http.NewRequest("GET", url, nil)
+func Request(target string) (*http.Response, RequestError) {
+	request, err := http.NewRequest("GET", target, nil)
 	if err != nil {
 		return nil, err
 	}
 	request.Header.Set("User-Agent", userAgent)
+
 	client := &http.Client{}
+	// client.CheckRedirect = func(req *http.Request, via []*http.Request) error {
+	//	return errors.New("Redirect")
+	// }
+
+	if options.withTor {
+
+		// fmt.Println("using tor... ")
+		tbProxyURL, err := url.Parse("socks5://127.0.0.1:9050")
+		if err != nil {
+			return nil, err
+		}
+		tbDialer, err := proxy.FromURL(tbProxyURL, proxy.Direct)
+		if err != nil {
+			return nil, err
+		}
+		tbTransport := &http.Transport{
+			Dial: tbDialer.Dial,
+		}
+		client.Transport = tbTransport
+	}
 
 	return client.Do(request)
 }
@@ -235,10 +275,14 @@ func HasElement(array []string, targets ...string) (bool, int) {
 func Investigo(username string, site string, data SiteData) Result {
 	var url, urlProbe string
 	result := Result{
-		exist:  false,
-		site:   site,
-		err:    true,
-		errMsg: "No return value",
+		Usernane: username,
+		URL:      data.URL,
+		URLProbe: data.URLProbe,
+		Proxied:  options.withTor,
+		Exist:    false,
+		Site:     site,
+		Err:      true,
+		ErrMsg:   "No return value",
 	}
 
 	// string to display
@@ -257,40 +301,87 @@ func Investigo(username string, site string, data SiteData) Result {
 			r.Body.Close()
 		}
 		return Result{
-			exist: false, site: site, err: true, errMsg: err.Error(),
+			Usernane: username,
+			URL:      data.URL,
+			URLProbe: data.URLProbe,
+			Proxied:  options.withTor,
+			Exist:    false,
+			Site:     site,
+			Err:      true,
+			ErrMsg:   err.Error(),
 		}
 	}
 
+	// check error types
 	switch data.ErrorType {
 	case "status_code":
 		if r.StatusCode <= 300 || r.StatusCode < 200 {
 			result = Result{
-				exist: true, link: url, site: site,
+				Usernane: username,
+				URL:      data.URL,
+				URLProbe: data.URLProbe,
+				Proxied:  options.withTor,
+				Exist:    true,
+				Link:     url,
+				Site:     site,
 			}
 		} else {
-			result = Result{site: site}
+			result = Result{
+				Site:     site,
+				Usernane: username,
+			}
 		}
 	case "message":
 		if !strings.Contains(ReadResponseBody(r), data.ErrorMsg) {
 			result = Result{
-				exist: true, link: url, site: site,
+				Usernane: username,
+				URL:      data.URL,
+				URLProbe: data.URLProbe,
+				Proxied:  options.withTor,
+				Exist:    true,
+				Link:     url,
+				Site:     site,
 			}
 		} else {
-			result = Result{site: site}
+			// check if 404
+			result = Result{
+				URL:      data.URL,
+				URLProbe: data.URLProbe,
+				Proxied:  options.withTor,
+				Usernane: username,
+				Site:     site,
+			}
 		}
 	case "response_url":
 		// In the original Sherlock implementation,
 		// the error type `response_url` works as `status_code`.
 		if (r.StatusCode <= 300 || r.StatusCode < 200) && r.Request.URL.String() == url {
 			result = Result{
-				exist: true, link: url, site: site,
+				Usernane: username,
+				URL:      data.URL,
+				URLProbe: data.URLProbe,
+				Proxied:  options.withTor,
+				Exist:    true,
+				Link:     url,
+				Site:     site,
 			}
 		} else {
-			result = Result{site: site}
+			result = Result{
+				Usernane: username,
+				URL:      data.URL,
+				URLProbe: data.URLProbe,
+				Proxied:  options.withTor,
+				Site:     site,
+			}
 		}
 	default:
 		result = Result{
-			exist: false, err: true, errMsg: "Unsupported error type `" + data.ErrorType + "`", site: site,
+			Usernane: username,
+			Proxied:  options.withTor,
+			Exist:    false,
+			Err:      true,
+			ErrMsg:   "Unsupported error type `" + data.ErrorType + "`",
+			Site:     site,
 		}
 	}
 
@@ -299,29 +390,45 @@ func Investigo(username string, site string, data SiteData) Result {
 	return result
 }
 
+// Check content of
+
 // WriteResult writes investigation result to stdout and file
 func WriteResult(result Result) {
 	if options.noColor {
-		if result.exist {
-			logger.Printf("[%s] %s: %s\n", ("+"), result.site, result.link)
+		if result.Exist {
+			logger.Printf("[%s] %s: %s\n", ("+"), result.Site, result.Link)
 		} else {
-			if result.err {
-				logger.Printf("[%s] %s: ERROR: %s", ("!"), result.site, (result.errMsg))
+			if result.Err {
+				logger.Printf("[%s] %s: ERROR: %s", ("!"), result.Site, (result.ErrMsg))
 			} else if options.verbose {
-				logger.Printf("[%s] %s: %s", ("-"), result.site, ("Not Found!"))
+				logger.Printf("[%s] %s: %s", ("-"), result.Site, ("Not Found!"))
 			}
 		}
 	} else {
-		if result.exist {
-			logger.Printf("[%s] %s: %s\n", color.HiGreenString("+"), color.HiWhiteString(result.site), result.link)
+		if result.Exist {
+			logger.Printf("[%s] %s: %s\n", color.HiGreenString("+"), color.HiWhiteString(result.Site), result.Link)
 		} else {
-			if result.err {
-				logger.Printf("[%s] %s: %s: %s", color.HiRedString("!"), result.site, color.HiMagentaString("ERROR"), color.HiRedString(result.errMsg))
+			if result.Err {
+				logger.Printf("[%s] %s: %s: %s", color.HiRedString("!"), result.Site, color.HiMagentaString("ERROR"), color.HiRedString(result.ErrMsg))
 			} else if options.verbose {
-				logger.Printf("[%s] %s: %s", color.HiRedString("-"), result.site, color.HiYellowString("Not Found!"))
+				logger.Printf("[%s] %s: %s", color.HiRedString("-"), result.Site, color.HiYellowString("Not Found!"))
 			}
 		}
 	}
 
 	return
+}
+
+func showBanner() {
+	banner := `
+                                              ."""-.
+                                             /      \
+ ____  _               _            _        |  _..--'-.
+/ ___|| |__   ___ _ __| | ___   ___| |__    >.` + "`" + `__.-""\;"` + "`" + `
+\___ \| '_ \ / _ \ '__| |/ _ \ / __| |/ /   / /(     ^\
+ ___) | | | |  __/ |  | | (_) | (__|   <    '-` + "`" + `)     =|-.
+|____/|_| |_|\___|_|  |_|\___/ \___|_|\_\    /` + "`" + `--.'--'   \ .-.
+                                           .'` + "`" + `-._ ` + "`" + `.\    | J /`
+
+	fmt.Printf("%v\n\n", banner)
 }
