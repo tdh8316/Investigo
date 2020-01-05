7484380ac07e0a96fd13d9fefc78c411db65db06
탁동혁 <tdh8316@naver.com>
2020-01-05 21:46:30 +0900

    Add generate_sites_md.py

---
+++ generate_sites_md.py
@@ -0,0 +1,8 @@
+import json
+
+site_dict: dict = json.loads(open("data.json", 'r', encoding="utf-8").read())
+    
+with open("sites.md", 'w', encoding="utf-8") as file:
+    file.write(f"# {len(site_dict)} sites are supprted!\n")
+    for site in sorted(site_dict.keys()):
+        file.write(f" - [{site}]({site_dict[site]['urlMain']})\n")
