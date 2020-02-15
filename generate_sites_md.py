f30e22a65efe0a3c9891bae5d9a6962df35ce2ac
탁동혁 <tdh8316@naver.com>
2020-02-15 14:58:54 +0900

    Add type hint for database structure

--- generate_sites_md.py
+++ generate_sites_md.py
@@ -1,8 +1,11 @@
+from typing import Dict, AnyStr
 import json
 
-site_dict: dict = json.loads(open("data.json", 'r', encoding="utf-8").read())
-    
-with open("sites.md", 'w', encoding="utf-8") as file:
+site_dict: Dict[AnyStr, AnyStr] = json.loads(
+    open("data.json", "r", encoding="utf-8").read()
+)
+
+with open("sites.md", "w", encoding="utf-8") as file:
     file.write(f"# {len(site_dict)} sites are supprted!\n")
     for site in sorted(site_dict.keys()):
         file.write(f" - [{site}]({site_dict[site]['urlMain']})\n")
