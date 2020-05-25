64b93047aa3a5f2d4f995f19063aa46f37a19106
탁동혁 <tdh8316@naver.com>
2020-05-25 21:58:21 +0900

    py: Fix typo

--- generate_sites_md.py
+++ generate_sites_md.py
@@ -6,7 +6,7 @@ site_dict: Dict[AnyStr, AnyStr] = json.loads(
 )
 
 with open("sites.md", "w", encoding="utf-8") as file:
-    file.write(f"# {len(site_dict)} sites are supprted!\n")
+    file.write(f"# {len(site_dict)} sites are supported!\n")
     for site in sorted(site_dict.keys()):
         file.write(f" - [{site}]({site_dict[site]['urlMain']})\n")
     file.write(
