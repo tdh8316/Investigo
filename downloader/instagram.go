68a60835b90c4c26b3c7f9fde2e9b066f99668a1
탁동혁 <tdh8316@naver.com>
2020-10-03 13:32:58 +0900

    Add goroutine and profile image downloader for instagram

--- downloader/instagram.go
+++ downloader/instagram.go
@@ -8,6 +8,7 @@ import (
 	"os"
 	"strconv"
 	"strings"
+	"sync"
 
 	"github.com/tidwall/gjson"
 )
@@ -15,29 +16,52 @@ import (
 func downloadInstagram(url string, logger *log.Logger) {
 	s := strings.Split(url, "/")
 	username := s[len(s)-1]
-	r, _ := http.Get(url + "?__a=1")
+	os.Mkdir(username, os.ModePerm)
 
+	r, _ := http.Get(url + "?__a=1")
 	bdB, _ := ioutil.ReadAll(r.Body)
+	r.Body.Close()
+
+	profilePicURLHd := gjson.GetBytes(bdB, "graphql.user.profile_pic_url_hd").String()
+	r, e := http.Get(profilePicURLHd)
+	if e != nil {
+		log.Fatal(e)
+	}
+	defer r.Body.Close()
+	file, err := os.Create(username + "/instagram_profile_pic_hd.jpg")
+	if err != nil {
+		log.Fatal(err)
+	}
+	_, err = io.Copy(file, r.Body)
+	if err != nil {
+		log.Fatal(err)
+	}
 
 	edges := gjson.GetBytes(bdB, "graphql.user.edge_owner_to_timeline_media.edges")
-
+	var wg sync.WaitGroup
 	for i, edge := range edges.Array() {
-		uri := edge.Get("node").Get("display_url").String()
-		r, e := http.Get(uri)
-		if e != nil {
-			log.Fatal(e)
-		}
-		defer r.Body.Close()
-		os.Mkdir(username, os.ModePerm)
-		file, err := os.Create(username + "/instagram_" + strconv.Itoa(i) + ".jpg")
-		if err != nil {
-			log.Fatal(err)
-			defer file.Close()
-		}
+		wg.Add(1)
+		go func(edge gjson.Result, i int) {
+			defer wg.Done()
+			uri := edge.Get("node").Get("display_url").String()
+			r, e := http.Get(uri)
+			if e != nil {
+				log.Fatal(e)
+			}
 
-		_, err = io.Copy(file, r.Body)
-		if err != nil {
-			log.Fatal(err)
-		}
+			file, err := os.Create(username + "/instagram_" + strconv.Itoa(i) + ".jpg")
+			if err != nil {
+				log.Fatal(err)
+			}
+
+			_, err = io.Copy(file, r.Body)
+			if err != nil {
+				log.Fatal(err)
+			}
+
+			r.Body.Close()
+			file.Close()
+		}(edge, i)
 	}
+	wg.Wait()
 }
