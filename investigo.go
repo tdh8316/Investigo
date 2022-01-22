054cd340c3449ace22d3590d1218465d15d72985
탁동혁 <tdh8316@naver.com>
2022-01-22 17:38:15 +0900

    Update User-Agent to the latest Chrome on Windows

--- investigo.go
+++ investigo.go
@@ -24,7 +24,7 @@ import (
 )
 
 const (
-	userAgent       string = "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/90.0.4430.93 Safari/537.36"
+	userAgent       string = "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/97.0.4692.99 Safari/537.36"
 	screenShotRes   string = "1024x768"
 	torProxyAddress string = "socks5://127.0.0.1:9050"
 )
